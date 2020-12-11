3a560368a845e34e50470ea9114e1364c17dae87
StickFunFight <36295961+StickFunFight@users.noreply.github.com>
2020-12-11 11:08:43 +0100

    Json ongebouwd

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,10 +7,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import sample.json.*;
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -54,36 +52,36 @@ public class Main extends Application {
     }
 
     public void GetCords() throws IOException, ParseException {
-        JSONParser parser = new JSONParser();
+        try {
 
-        URL oracle = new URL("https://bp6.adainforma.tk/helloworldbot/functions/datalayer/api/obstacle/?selector=ae026dd58cd57fd2&validator=4424bdd85905aa88646327911b6893598a279abb4f82466dca61a988041afb08&action=get");
-        URLConnection yc = oracle.openConnection();
-        BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+            URL oracle = new URL("https://bp6.adainforma.tk/helloworldbot/functions/datalayer/api/obstacle/?selector=ae026dd58cd57fd2&validator=4424bdd85905aa88646327911b6893598a279abb4f82466dca61a988041afb08&action=get");
+            URLConnection yc = oracle.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
 
-        String inputLine;
-        while ((inputLine = in.readLine()) != null) {
-            JSONArray a = (JSONArray) parser.parse(inputLine);
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                JsonArray json = Json.parse(inputLine).asArray();
 
-            for (Object o : a) {
+                for (Object o : json) {
 
-                JSONObject Cords = (JSONObject) o;
-                //haal de eerste coords op.
-                String Row = (String) Cords.get("row1");
-                String Column = (String) Cords.get("column1");
-                //haal de 2e coords op.
-                String Row2 = (String) Cords.get("row2");
-                String Column2 = (String) Cords.get("column2");
+                    JsonObject Cords = (JsonObject) o;
+                    //haal de eerste coords op.
+                    JsonValue Row = Cords.get("row1");
+                    JsonValue Column = Cords.get("column1");
+                    //haal de 2e coords op.
+                    JsonValue Row2 = Cords.get("row2");
+                    JsonValue Column2 = Cords.get("column2");
 
-                int iRow = Integer.parseInt(Row);
-                int iColum = Integer.parseInt(Column);
-                int iRow2 = Integer.parseInt(Row2);
-                int iColum2 = Integer.parseInt(Column2);
+                    int iRow = Integer.parseInt(String.valueOf(Row));
+                    int iColum = Integer.parseInt(String.valueOf(Column));
+                    int iRow2 = Integer.parseInt(String.valueOf(Row2));
+                    int iColum2 = Integer.parseInt(String.valueOf(Column2));
 
-                blocksArray.add(new int[]{iRow, iColum});
-                blocksArray.add(new int[]{iRow2, iColum2});
-
-                //TODO er voor zorgen dat alles word toegevoegd.
+                    System.out.println(iRow);
+                }
             }
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
 
